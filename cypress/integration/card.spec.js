0e3a8ffec95e30196d89a206bba614f21e2a158b
imked <imke.duensing@gmail.com>
2019-03-05 11:18:32 +0100

    Create card.spec.js

--- cypress/integration/card.spec.js
+++ cypress/integration/card.spec.js
@@ -2,16 +2,24 @@ describe('React App', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
+
   describe('/', () => {
     it('has the correct title', () => {
       cy.title().should('include', 'Morning App')
     })
   })
 
-  it('has a card', () => {
-    cy.get('card').should('have.length', 1)
-    cy.get('card > h3').should('have.length', 1)
-    cy.get('card > p').should('have.length', 1)
-    cy.get('card > img').should('have.length', 1)
+  describe('section', () => {
+    it('has a card', () => {
+      cy.get('section')
+        .find('h3')
+        .should('have.length', 1)
+      cy.get('section')
+        .find('p')
+        .should('have.length', 1)
+      cy.get('section')
+        .find('img')
+        .should('have.length', 1)
+    })
   })
 })
